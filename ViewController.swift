e23eae0a13c3d58009aacf1253479b9ebe5cd288
Herman Saini <herman.saini@gmail.com>
2022-01-12 18:01:20 -0500

    New Features
    Added the Split Feature and added some colors to the labels

--- ViewController.swift
+++ ViewController.swift
@@ -16,11 +16,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var numOfPeopleTextField: UITextField!
+    
+    @IBOutlet weak var totalPerPersonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    var splitter = false;
     
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
@@ -34,6 +39,27 @@ class ViewController: UIViewController {
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalAmountLabel.text = String(format: "$%.2f", total)
         
+        if(splitter){
+            let numPeople = Double(numOfPeopleTextField.text!) ?? 0
+            totalPerPersonLabel.text = String(format: "$%.2f", total/numPeople)
+        }
+    }
+    
+
+    @IBAction func calculateSplit(_ sender: Any) {
+        splitter = true
+        
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = tip + bill
+        
+        let numPeople = Double(numOfPeopleTextField.text!) ?? 0
+        
+        totalPerPersonLabel.text = String(format: "$%.2f", total/numPeople)
     }
     
 }
